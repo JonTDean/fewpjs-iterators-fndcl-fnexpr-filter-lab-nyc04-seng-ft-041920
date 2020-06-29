7c542f395e0b05759335e993dc2dae646a6735c1
JonTDean <JonathanTomDean@Gmail.com>
2020-06-29 07:03:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(driver => driver.toUpperCase() == string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => driver.charAt(0).toUpperCase() == string.charAt(0).toUpperCase());
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name == string);
+}
